66dcb567eaca7264deea7b8396190cb0572bdb6e
Hugo Messias <hugomessias9@gmail.com>
2025-01-09 20:05:01 -0300

    Created details page - Implemented `details` in `polls/views.py` and created `details.html` - Updated `views.py` to use the `render` shortcut - Updated `index.html` to remove the hardcoded URL using the `{% url %}` template tag - Added `app_name` to `polls/urls.py` for compatibility with url namespaces

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
+from django.shortcuts import render
 
 from .models import Question, Choice
 
 
 def index(request):
     latest_questions = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render({"question_list": latest_questions}, request))
+    context = {"question_list": latest_questions}
+    return render(request, "polls/index.html", context)
 
 
 def details(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("This question doesn't exist or is no longer avaliable")
+    context = {"question": question}
+    return render(request, "polls/details.html", context)
 
 
 def results(request, question_id):
