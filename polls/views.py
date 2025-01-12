64ac4ace927babf19d0798cfc6ce187340023236
Hugo Messias <hugomessias9@gmail.com>
2025-01-11 22:56:59 -0300

    Implement voting functionality - Update `polls/detaisl.html` to include a form - Implement `vote` function in `views.py`

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
+from django.urls import reverse
 from django.template import loader
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Choice
 
@@ -26,4 +28,22 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            context={
+                "question": question,
+                "error_message": "Please select one of the options below.",
+            },
+        )
+    else:
+        choice.votes = F("votes") + 1  # Performs the update directly on the database
+        choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
