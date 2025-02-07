930cbbd42172d831f18db6a652d303c9fbf62e64
Hugo Messias <hugomessias9@gmail.com>
2025-02-06 21:47:46 -0300

    Customize admin site for the `Question` model

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,32 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# class that will be used to display a model as part of another model on the create/edit page
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # defines all the information that will be displayed on the page listing all the entries on the data base
+    # also allows sorting by any of these values (although arbitrary functions need some extra steps for that)
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    # associate field groups of the model to named sections on the create/edit page
+    fieldsets = (
+        ("Question Details", {"fields": ["question_text"]}),
+        ("Date", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    )
+
+    # reference to another model that should be rendered as a section of the current model on the create/edit page
+    inlines = [ChoiceInLine]
+
+    # allows entries to be filtered by the fields in the list (the filter is based on the type of the function)
+    list_filter = ["pub_date"]
+
+    # allows searches on the specified fields
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 
@@ -8,6 +9,12 @@ class Question(models.Model):
     question_text = models.CharField("question", max_length=200)
     pub_date = models.DateTimeField("date published", default=timezone.now)
 
+    # information used by the admin site
+    @admin.display(
+        boolean=True,  # makes the field be displayed as boolean
+        ordering="pub_date",  # how to oreder entries by this function
+        description="Was published recently?",  # name of the function on the admin page
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
